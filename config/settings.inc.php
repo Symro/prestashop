13bd5f2f16e234d0ee3e848e5e09dae57cb62ba4
fgrandval <florent.grandval@live.fr>
2015-04-01 19:06:41 +0200

    Modifs gitignore, ajout settings

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'cocktail_shop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'e2TfW4Xu1avu1XoXfKkUdGF5V6yqBWnzLx7eiY2NFaaU5PKasEDWW9OH');
+define('_COOKIE_IV_', 'tBHVqDfG');
+define('_PS_CREATION_DATE_', '2015-03-31');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.0.14');
+define('_RIJNDAEL_KEY_', 'eryCLttSy77cmJwNR4tTnKxCfJARUCJa');
+define('_RIJNDAEL_IV_', 'rfhrv+nYbH9xA+wNjN70kA==');
